fdfa3f4459636b1de76204ec4837d14dab7606e3
HEMANT KAKRAN <hemantkakran100@gmail.com>
2026-07-01 23:38:34 +0530

    Time: 3 ms (78.15%), Space: 46.3 MB (75.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,39 @@
 class Solution {
-    public int romanToInt(String s) {
-        int ans = 0, num = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
-            }
-            if (4 * num < ans)
-                ans -= num;
-            else
-                ans += num;
+    public int getIntValue(char roman) {
+        switch (roman) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            default: return 1000;
         }
-        return ans;
+    }
 
+
+    public int romanToInt(String s) {
+        char[] arr = s.toCharArray();
+        int l = arr.length - 1;
+        int prev = 0;
+        int ans = 0;
+        
+        
+        for (int idx = l; idx >= 0; idx--) {
+            char romanChar = arr[idx];
+            int intVal = getIntValue(romanChar);
+            
+            
+            if (intVal >= prev)
+                ans += intVal;
+            else
+                ans -= intVal;
+            
+            
+            prev = intVal;
+        }
+        
+        
+        return ans;
     }
 }
